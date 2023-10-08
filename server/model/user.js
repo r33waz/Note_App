cb7f590f834e9443597ddf14babcdad0e4f4596f
Riwaz Thapa <reewazthapa22@gmail.com>
2023-10-08 12:23:12 +0545

    first commit

---
+++ server/model/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+
+const UserSchema = new Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  profileImage: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+export default User
